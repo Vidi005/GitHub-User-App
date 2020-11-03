89163aa528144848e5ca1981d86f171d2bbe5efb
Vidi Fitriansyah Hidarlan <55016463+Vidi005@users.noreply.github.com>
2020-11-03 21:58:45 +0700

    Update Following.kt

--- GitHubUserApp/Submission 2/app/src/main/java/com/dicoding/picodiploma/githubuseruiuxapi/model/Following.kt
+++ GitHubUserApp/Submission 2/app/src/main/java/com/dicoding/picodiploma/githubuseruiuxapi/model/Following.kt
@@ -1,1 +1,11 @@
-package com.dicoding.picodiploma.githubuseruiuxapi.modelimport android.os.Parcelableimport kotlinx.android.parcel.Parcelize@Parcelizedata class Following(var avatar: String?,                     var username: String?,                     var id: Int?,                     var type: String?) : Parcelable
+package com.dicoding.picodiploma.githubuseruiuxapi.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Following(val avatar: String?,
+                     val username: String?,
+                     val id: Int?,
+                     val type: String?
+) : Parcelable
